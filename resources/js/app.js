b577ac07bdf513ec3841f8772b33096e159d8a03
Sakamoto0525 <525masaya@gmail.com>
2019-12-28 09:47:44 +0900

    fix eslint

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,12 +1,12 @@
-import Vue from 'vue'
+import Vue from "vue";
 // ルーティングの定義をインポートする
-import router from './router'
+import router from "./router";
 // ルートコンポーネントをインポートする
-import App from './App.vue'
+import App from "./App.vue";
 
 new Vue({
-  el: '#app',
-  router, // ルーティングの定義を読み込む
-  components: { App }, // ルートコンポーネントの使用を宣言する
-  template: '<App />' // ルートコンポーネントを描画する
-})
+    el: "#app",
+    router, // ルーティングの定義を読み込む
+    components: { App }, // ルートコンポーネントの使用を宣言する
+    template: "<App />" // ルートコンポーネントを描画する
+});
